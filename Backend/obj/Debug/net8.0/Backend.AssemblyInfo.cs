fba57ca6a89499f9477ffc19973ad506e01554e2
yanti <Yanti@LAPTOP-SGL1921R>
2024-07-28 14:22:21 +0700

    first commit

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06981ca0142cb1d3c70404c8b10d90d938956561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+232061b4f789ff0c5cc52081ce619423c5459dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
